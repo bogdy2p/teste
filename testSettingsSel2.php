43597d6dd9aef9dc3aa853058295607a00452453
Popa Bogdan Constantin <bogdan.popa@reea.net>
2016-03-18 15:32:26 +0200

    Test settings with selenium2

--- testSettingsSel2.php
+++ testSettingsSel2.php
@@ -30,33 +30,6 @@ class ExampleTest extends PHPUnit_Extensions_Selenium2TestCase
         }
     }
 
-    public function fillBillingAdressForm()
-    {
-
-        $sexSelector           = '#billing-new-address-form > fieldset > ul > li:nth-child(1) > div > div.field.name-prefix > div > input[type="radio"]:nth-child(1)';
-        sleep(1);
-        $this->byCssSelector($sexSelector)->click();
-        sleep(1);
-        $this->byName('billing[firstname]')->value('FirstnameValueHere');
-        $this->byName('billing[lastname]')->value('LastnameValueHere');
-        $this->byName('billing[street][]')->value('StreetValueHere');
-        $this->byName('billing[postcode]')->value('8085');
-        sleep(1);
-        $this->byName('billing[city]')->value('CityName');
-        sleep(1);
-        $this->byName('billing[email]')->value('bogdan.popa@reea.net');
-        sleep(1);
-        $this->byName('billing[use_for_shipping]')->value('1');
-        sleep(1);
-        $paymentMethodSelector = '#p_method_banktransfer';
-        $this->byCssSelector($paymentMethodSelector)->click();
-        sleep(5);
-
-        $economyShippingSelector = '#s_method_matrixrate_matrixrate_3';
-        $economyShipping         = $this->findElementWaitUntilbyCssPath($economyShippingSelector);
-        $economyShipping->click();
-    }
-
     public function testMyTestCase()
     {
 
@@ -69,7 +42,7 @@ class ExampleTest extends PHPUnit_Extensions_Selenium2TestCase
         $this->byName('login[password]')->value('admin123');
         $this->byId('loginForm')->submit();
 
-        sleep(1);
+//        sleep(1);
         //Close details if exist (Try/Catch)
         try {
             $popUpCloseSelector = '#message-popup-window > div.message-popup-head > a';
@@ -84,54 +57,87 @@ class ExampleTest extends PHPUnit_Extensions_Selenium2TestCase
         $languageSelector = $this->byId("interface_locale");
         $this->select($languageSelector)->selectOptionByValue('en_US');
 
-        
-        
-        sleep(15);
-        //Get xpath for the item on the product page , and click it.
-//        $xpathForFirstItem = '//*[@id="amshopby-page-container"]/div[3]/ul/li[1]/div/div[1]/a';
-//        $this->byXPath($xpathForFirstItem)->click();
-//        //Sleep for 1 seconds
-//        sleep(1);
-//        $superAttributeOptionsPath = "//*[contains(@class, 'super-attribute-select')]";
-//        //Initialize path for product options button here:
-//        $addToCartSelector = '#product_addtocart_form > div.row > div.product-shop-right.col-sm-4 > div.add-to-box > div.add-to-cart > button';
-//
-//        try {
-//            $selectDiv         = $this->byXPath($superAttributeOptionsPath);
-//            $selectDiv->click();
-//            $this->keys(Keys::ARROW_DOWN);
-//            $this->keys(Keys::ARROW_DOWN);
-//            $this->keys(Keys::ENTER);
-//            $addToCartSelector = '#product_addtocart_form > div.row > div.product-shop-right.col-sm-4 > div.product-options-bottom > div.add-to-cart > button';
-//        } catch (Exception $ex) {
-//
-//        }
-//        sleep(1);
-//        //Add selected product (with option if available , to cart)
-//        $this->byCssSelector($addToCartSelector)->click();
-//        sleep(1);
-//
-//        $zurKasseSelector = 'body > div.wrapper > div > div.header-container.type4 > div.header.container > div.cart-area > div.mini-cart > div > div > div.actions > a';
-//
-//        $this->byCssSelector($zurKasseSelector)->click();
-//        sleep(1);
-//
-//        $this->fillBillingAdressForm();
-//
-//        $this->clickGiftWrapAndWait(10);
-        //Click Place Order
-//        $checkoutButton = $this->byCssSelector('#checkout-review-submit > button > span > span');
-//
-//        try {
-//            $resultz = $checkoutButton->click();
-//        } catch (Exception $ex) {
-//            var_dump($ex->getMessage());
-//        }
-//        //Wait 10 seconds.
-//        sleep(15);
-//
-//        //Assert that the url we were redirected to contains the success.
-//        $this->assertContains('onepage/success', $this->url());
+
+        $systemMenuSelector = '#nav > li:nth-child(12) > a > span';
+        $this->byCssSelector($systemMenuSelector)->click();
+
+        //Go-to Configuration page
+        $configurationSelector = '#nav > li:nth-child(12) > ul > li:nth-child(21) > a > span';
+        $this->byCssSelector($configurationSelector)->click();
+
+        //Click on general -> store email adresses;
+
+        $storeEmailAdressesSelector = '#system_config_tabs > li:nth-child(4) > dl > dd:nth-child(6) > a';
+        $this->byCssSelector($storeEmailAdressesSelector)->click();
+
+        //Click on each of the 5 links to expand email settings
+        $email_fields = array(
+            'trans_email_ident_general-head',
+            'trans_email_ident_sales-head',
+            'trans_email_ident_support-head',
+            'trans_email_ident_custom1-head',
+            'trans_email_ident_custom2-head'
+        );
+
+        foreach ($email_fields as $emailfield) {
+            $this->byId($emailfield)->click();
+//            sleep(1);
+        }
+
+        //Assert that store emails are set to bogdan.popa@reea.net
+
+        $email_inputs = array(
+            'trans_email_ident_general_email',
+            'trans_email_ident_sales_email',
+            'trans_email_ident_support_email',
+            'trans_email_ident_custom1_email',
+            'trans_email_ident_custom2_email'
+        );
+
+        foreach ($email_inputs as $email_input_field) {
+            $this->assertEquals('bogdan.popa@reea.net',
+                $this->byId($email_input_field)->value());
+        }
+
+        sleep(2);
+
+        $generalContactsSelector = '#system_config_tabs > li:nth-child(4) > dl > dd:nth-child(7) > a';
+        $this->byCssSelector($generalContactsSelector)->click();
+
+        $contacts_fields = array(
+            'contacts_contacts-head',
+            'contacts_email-head'
+        );
+
+        foreach ($contacts_fields as $contactsfield) {
+            $this->byId($contactsfield)->click();
+            sleep(1);
+        }
+
+        $contacts_inputs = array(
+            'contacts_email_recipient_email'
+        );
+
+        foreach ($contacts_inputs as $contacts_input) {
+            $this->assertEquals('bogdan.popa@reea.net',
+                $this->byId($contacts_input)->value());
+        }
+
+        sleep(1);
+        //Go to SALES->Sales Emails tab.
+
+        $salesSalesEmailsSelector = '#system_config_tabs > li:nth-child(20) > dl > dd:nth-child(3) > a';
+        $this->byCssSelector($salesSalesEmailsSelector)->click();
+
+        $salesEmailInvoiceID = 'sales_email_invoice-head';
+        //Check if sales email invoice tab is open // If not open , open it !
+         if (!$this->byId('sales_email_invoice_copy_to')) {
+                $this->byId($salesEmailInvoiceID)->click();
+            }
+       
+        sleep(1);
+        $this->assertEquals('bogdan.popa@reea.net', $this->byId('sales_email_invoice_copy_to')->value());
+        sleep(1);
     }
 
     public function tearDown()
